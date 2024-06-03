1d0c503d1feb756dc0865dbdef56127093f72df5
Faister Cabrera Carvalho <faister.carvalho@toptal.com>
2024-06-03 20:28:24 -0300

    log request options

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var postMessage = function(message, callback) {
   };
   console.info("sending message", message);
   console.info("to", hookUrl);
+  console.info("with options", options);
 
   var postReq = https.request(options, function(res) {
     var chunks = [];
